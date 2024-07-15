f797a7e3d92832c9df07400443f871ef7c7e2bbe
Ian Ale <ianalebom@gmail.com>
2024-07-16 02:48:29 +0700

    second

--- components/component/component.tsx
+++ components/component/component.tsx
@@ -37,6 +37,15 @@ const HomePage = () => {
                 <p className="max-w-[600px] text-muted-foreground md:text-xl">
                   Im a backend developer with a passion for creating a web applications.
                 </p>
+                <Link
+  href="/CV.pdf"
+  className="inline-flex h-10 items-center justify-center rounded-md bg-primary px-6 text-sm font-medium text-primary-foreground shadow transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
+  prefetch={false}
+  download
+>
+  Download CV
+</Link>
+
               </div>
             </div>
             <div className="flex justify-center">
